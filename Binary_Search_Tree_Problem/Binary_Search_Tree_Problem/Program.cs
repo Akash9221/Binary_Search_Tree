4eb26660b176f66da4470ceb6aa830af45a7a114
“akashmane3938” <maneakash3938@gmail.com>
2022-12-08 01:28:45 -0800

    Search Element IN Binary Tree

--- Binary_Search_Tree_Problem/Binary_Search_Tree_Problem/Program.cs
+++ Binary_Search_Tree_Problem/Binary_Search_Tree_Problem/Program.cs
@@ -15,6 +15,24 @@ namespace Binary_Search_Tree_Problem
             //Console.ReadLine();
 
             //BST2
+            //BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            //bst.Insert(30);
+            //bst.Insert(70);
+            //bst.Insert(22);
+            //bst.Insert(40);
+            //bst.Insert(60);
+            //bst.Insert(95);
+            //bst.Insert(11);
+            //bst.Insert(65);
+            //bst.Insert(3);
+            //bst.Insert(16);
+            //bst.Insert(63);
+            //bst.Insert(67);
+            //bst.Display();
+            //Console.WriteLine("Size Of Binary Search Tree Is:" + bst.size());
+            //Console.ReadLine();
+
+            //BST3
             BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
             bst.Insert(30);
             bst.Insert(70);
@@ -30,6 +48,8 @@ namespace Binary_Search_Tree_Problem
             bst.Insert(67);
             bst.Display();
             Console.WriteLine("Size Of Binary Search Tree Is:" + bst.size());
+            bst.IfExists(63, bst);
+
             Console.ReadLine();
         }
     }
