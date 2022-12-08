92d22789f4715309574c1f712a1922c96fc4131d
“akashmane3938” <maneakash3938@gmail.com>
2022-12-08 01:08:34 -0800

    Create Binary Tree

---
+++ Binary_Search_Tree_Problem/Binary_Search_Tree_Problem/BST2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree_Problem
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData
+        {
+            get; set;
+        }
+        public BinarySearchTree<T> LeftTree { get; set; }   
+        public BinarySearchTree<T> RightTree { get; set; }
+
+        public BinarySearchTree(T nodedata)
+        {
+            this.NodeData = nodedata;
+            this.LeftTree = null;
+            this.RightTree = null;  
+        }
+        static int lcnt = 0;
+        static int rcnt = 0;
+
+        public void Insert(T data)
+        {
+            T CurrentNodeValue = this.NodeData;
+            if((CurrentNodeValue.CompareTo(data))>0)
+            {
+                if(this.LeftTree==null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(data);
+                }
+                else
+                {
+                    this.LeftTree.Insert(data); 
+                }
+            }
+            else
+            {
+                if(this.RightTree==null)
+                {
+                    this.RightTree=new BinarySearchTree<T>(data);   
+                }
+                else
+                {
+                    this.RightTree.Insert(data);    
+                }
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                lcnt++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                rcnt++;
+                this.RightTree.Display();
+            }
+        }
+        public int size()
+        {
+            return (1 + lcnt + rcnt);
+        }
+    }
+}
--- Binary_Search_Tree_Problem/Binary_Search_Tree_Problem/Program.cs
+++ Binary_Search_Tree_Problem/Binary_Search_Tree_Problem/Program.cs
@@ -6,12 +6,30 @@ namespace Binary_Search_Tree_Problem
     {
         static void Main(string[] args)
         {
+            //BST1
+            //BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            //bst.Insert(30);
+            //bst.Insert(70);
+            //bst.Display();
+            //Console.ReadLine();
+            //Console.ReadLine();
 
+            //BST2
             BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
             bst.Insert(30);
             bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(11);
+            bst.Insert(65);
+            bst.Insert(3);
+            bst.Insert(16);
+            bst.Insert(63);
+            bst.Insert(67);
             bst.Display();
-            Console.ReadLine();
+            Console.WriteLine("Size Of Binary Search Tree Is:" + bst.size());
             Console.ReadLine();
         }
     }
